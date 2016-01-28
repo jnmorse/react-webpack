155cc38ed9cd2868f06b7ebe1e7f127224fa6714
Joseph Morse <tamed.lionheart@gmail.com>
2016-01-28 10:24:06 -0500

    Added Changes For Hot Reloading
    Not sure the change to preLoaders was needed, but just something else I
    did.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,51 @@
-var autoprefixer = require('autoprefixer');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var app = path.join(__dirname, 'app');
 
 module.exports = {
   entry: {
-    app: './app/scripts/main.js',
-    style: './app/styles/styles.js',
-    vendor: ['react', 'react-dom', 'jquery']
+    app: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './app/scripts/main'
+    ],
   },
   output: {
-    path: './build',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
     filename: './[name].js',
     chunkFilename: './[id].js'
   },
+  devtool: 'eval',
   module: {
     preLoaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'source-map-loader'
       }
     ],
     loaders: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!postcss!sass')
+        loader: 'style!css!postcss!sass'
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader']
+        loaders: ['react-hot', 'babel']
+
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
       }
     ]
+  },
+  sassLoader: {
+    includePaths: [path.join(__dirname, 'bower_components')]
   },
   postcss: function () {
     return {
@@ -40,12 +54,7 @@ module.exports = {
     };
   },
   plugins: [
-    new ExtractTextPlugin('style', 'css/[name].css'),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'js/vendor.js'),
-    new HtmlWebpackPlugin({
-      title: 'ReactStarter',
-      template: 'app/index.html',
-      inject: 'body'
-    })
+    new webpack.optimize.CommonsChunkPlugin(['app', 'vendor'], 'js/[name].js'),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
