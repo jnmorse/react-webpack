d7b37d75f6f8e984e535e818bf78b85901fd689d
Joseph Morse <tamed.lionheart@gmail.com>
2016-05-02 22:03:16 -0400

    Updated Test Script
    All test now pass

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@ var app = require('../server')
 
 describe('Request to the root path', function() {
   it('Returns a 200 status code', function(done) {
+    this.timeout(5000)
+
     request(app)
       .get('/')
       .expect(200, done)
